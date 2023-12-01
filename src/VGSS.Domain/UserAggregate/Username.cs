6c03eeee107f3ba2856f1eab04521eaeba308bfc
Ken <ken@vgss.io>
2023-12-01 18:58:06 +0100

    Remove the ValueObject base class in favor for readonly record structs.

--- src/VGSS.Domain/UserAggregate/Username.cs
+++ src/VGSS.Domain/UserAggregate/Username.cs
@@ -1,20 +1,2 @@
 ﻿namespace VGSS.Domain.UserAggregate;
-public sealed class Username : ValueObject
-{
-    public string Value { get; }
-
-    public Username(string value)
-    {
-        Value = value;
-    }
-
-    protected override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return Value;
-    }
-
-    public static implicit operator string(Username username)
-    {
-        return username.Value;
-    }
-}
+public readonly record struct Username(string Value);
